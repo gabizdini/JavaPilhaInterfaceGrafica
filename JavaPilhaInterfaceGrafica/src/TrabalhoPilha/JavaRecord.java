84ecb014d10b48c9cb2103b87f04aa204188a957
Gabrieli Diniz <128835174+gabizdini@users.noreply.github.com>
2024-04-02 17:33:38 -0300

    Add files via upload
    first commit

---
+++ JavaPilhaInterfaceGrafica/src/TrabalhoPilha/JavaRecord.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrabalhoPilha;
+
+/**
+ *
+ * @author mefma
+ */
+public class JavaRecord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormSystem f = new FormSystem();
+        f.setVisible(true);
+    }
+    
+}
